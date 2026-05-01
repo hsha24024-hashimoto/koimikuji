2ba7c91a450a3320f096ba99eeab9d533d2d4a21
Hashimoto <Hashimoto@akiha.com>
2026-05-01 12:51:05 +0900

    結果で文字の色を変更

--- script.js
+++ script.js
@@ -6,10 +6,21 @@ const fortunes =[
     "小吉　今日はゆっくり過ごそう",
     "凶　無理せず慎重に行動しよう",
 ];
-const button = document.grtElementById("omikuji-bt");
-const result = document.grtElementById("result");
+const button = document.getElementById("omikuji-bt");
+const result = document.getElementById("result");
 
 button.addEventListener("click",function () {
+
+
 const randomNumber = Math.floor(Math.random() * fortunes.lengt);
+const resultText =fortunes[(randomnumber)]
+
 result.textContent = fortunes[(randomNumber)];
-});
+
+if (resultText.includes("大吉")) {
+     result.style.color = "red";
+}else if(resultText.includes("中吉")) {
+     result.style.color = "green" ;
+}else {
+  (result .style.color = "black")
+}});
